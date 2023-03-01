04dbbe4692f98dbc9e799049c162ab98ec125810
Ron Ben david <100694593+RonBenDavid@users.noreply.github.com>
2023-03-01 19:17:33 +0200

    Delete Nu-LB-NUC140_UART0_HC05 directory

--- Nu-LB-NUC140_UART0_HC05/obj/ascii_table.d
+++
@@ -1,1 +0,0 @@
-.\obj\ascii_table.o: ..\..\..\Src\NUC1xx-LB_002\Ascii_Table.C
--- Nu-LB-NUC140_UART0_HC05/obj/core_cm0.d
+++
@@ -1,2 +0,0 @@
-.\obj\core_cm0.o: ..\..\..\..\CMSIS\CM0\CoreSupport\core_cm0.c
-.\obj\core_cm0.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
